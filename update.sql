5072dfa96580203756b9690029cdcbcb2958ebc3
IreneG <100474230@alumnos.uc3m.es>
2024-11-10 12:21:42 +0100

    Delete update.sql

--- update.sql
+++
@@ -1,4 +0,0 @@
-SET SQL_SAFE_UPDATES = 0;
-UPDATE customers SET cust_email = "ppicasso@gmail.com" WHERE cust_id = 1;
-UPDATE customers SET cust_email = "lincoln@us.gov" WHERE cust_id = 2;
-UPDATE customers SET cust_email = "hello@napoleon.me" WHERE cust_id = 3;
